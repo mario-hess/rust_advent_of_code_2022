60eaeb2c1432718a40a69b89444d4738e89255b2
Mario Hess <office.hess@gmail.com>
2022-12-09 14:07:00 +0100

    :sparkles: Add day 3 part 2

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs::*;
 use std::io::*;
 
-#[derive(Debug)]
+#[derive(PartialEq)]
 struct Rucksack {
     items: Vec<char>,
     compartment_one: Vec<char>,
@@ -18,10 +18,112 @@ impl Rucksack {
     }
 }
 
-fn main() {
-    let file = File::open("./src/input.txt").expect("Unable to open file.");
-    let lines = BufReader::new(file).lines();
+#[derive(Clone)]
+struct Group {
+    items: Vec<Vec<char>>,
+}
 
+impl Group {
+    fn new() -> Self {
+        Self { items: Vec::new() }
+    }
+}
+
+fn main() {
+    let path = "./src/input.txt";
+
+    // Part one
+    let lines = get_lines(path);
+    let collection = get_matching_items(lines);
+    let sum = calculate_priorities(collection);
+
+    println!("Sum: {}", sum);
+
+    // Part two
+    let lines = get_lines(path);
+    let groups = get_groups(lines);
+    let chars = get_group_match(groups.clone());
+    let sum = calculate_priorities(chars);
+
+    println!("Sum: {}", sum);
+}
+
+fn get_group_match(groups: Vec<Group>) -> Vec<char> {
+    let mut matched_list: Vec<char> = Vec::new();
+
+    for group in groups {
+        let first: Vec<char> = group.items[0].clone();
+        let second: Vec<char> = group.items[1].clone();
+        let third: Vec<char> = group.items[2].clone();
+
+        let mut possible_match: Vec<char> = Vec::new();
+        let mut matched_char: char = ' ';
+
+        for items in first.clone() {
+            for item in second.clone() {
+                if item == items {
+                    possible_match.push(item);
+                }
+            }
+        }
+
+        for items in possible_match {
+            for item in third.clone() {
+                if item == items {
+                    matched_char = item;
+                    break;
+                }
+            }
+        }
+
+        matched_list.push(matched_char);
+    }
+
+    matched_list
+}
+
+fn get_groups(lines: Lines<BufReader<File>>) -> Vec<Group> {
+    let mut groups: Vec<Group> = Vec::new();
+    let mut group: Group = Group::new();
+
+    let mut counter = 0;
+    for line in lines {
+        let mut l: Vec<char> = Vec::new();
+
+        for c in line.unwrap().chars() {
+            l.push(c);
+        }
+
+        counter += 1;
+        group.items.push(l);
+
+        if counter == 3 {
+            groups.push(group.clone());
+            group.items.clear();
+            counter = 0;
+        }
+    }
+
+    groups
+}
+
+fn calculate_priorities(collection: Vec<char>) -> u32 {
+    let mut sum = 0;
+
+    for item in collection {
+        let i = item as u32;
+
+        if i < 97 {
+            sum += i - 38;
+        } else {
+            sum += i - 96;
+        }
+    }
+
+    sum
+}
+
+fn get_matching_items(lines: Lines<BufReader<File>>) -> Vec<char> {
     let rucksacks = sort_compartments(get_rucksacks(lines));
 
     let mut collection: Vec<char> = Vec::new();
@@ -38,23 +140,8 @@ fn main() {
         }
         collection.push(result);
     }
-    
-    collection.sort();
 
-    let mut sum = 0;
-
-    for item in collection {
-        let i = item as u32;
-        
-        if i < 97 {
-            sum += i - 38;
-        }
-        else {
-            sum += i - 96; 
-        }    
-    }
-
-    println!("Sum: {}", sum);
+    collection
 }
 
 fn sort_compartments(rucksacks: Vec<Rucksack>) -> Vec<Rucksack> {
@@ -93,4 +180,11 @@ fn get_rucksacks(lines: Lines<BufReader<File>>) -> Vec<Rucksack> {
     }
 
     rucksacks
+}
+
+fn get_lines(path: &str) -> Lines<BufReader<File>> {
+    let file = File::open(path).expect("Unable to open file.");
+    let lines = BufReader::new(file).lines();
+
+    lines
 }
