9a7bc0685b33ea46ecd39ef965d969f180c657c4
Mario Hess <office.hess@gmail.com>
2022-12-13 00:26:23 +0100

    :tada: Add part 2

--- improved_day_1/src/main.rs
+++ improved_day_1/src/main.rs
@@ -1,13 +1,29 @@
+use itertools::Itertools;
+use std::cmp::Reverse;
+
 fn main() {
+    // Part one
     let lines = include_str!("input.txt")
         .lines()
         .map(|v| v.parse::<u64>().ok())
         .collect::<Vec<_>>();
 
-    let max = lines
+    let answer = lines
         .split(|line| line.is_none())
         .map(|group| group.iter().map(|v| v.unwrap()).sum::<u64>())
-        .max();
+        .max().unwrap();
 
-    println!("Max = {max:?}");
+    println!("Answer: {answer:?}");
+
+    // Part two
+    let answer = include_str!("input.txt")
+        .lines()
+        .map(|v| v.parse::<u64>().ok())
+        .batching(|mut it| (&mut it).map_while(|x| x).sum1::<u64>())
+        .map(Reverse)
+        .k_smallest(3)
+        .map(|x| x.0)
+        .sum::<u64>();
+
+    println!("Answer: {answer:?}");
 }
